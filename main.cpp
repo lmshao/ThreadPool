b1c170c43d17b6a1a6d80a9196557c76accab982
SHAO Liming <lmshao@163.com>
2021-04-10 17:30:38 +0800

    change directory

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
-#include "ThreadPool.h"
 #include <unistd.h>
 #include <iostream>
-
-void *task(void *arg)
-{
-    int *param = (int *)arg;
-    printf("[%lu] --> Working on task %d\n", pthread_self(), *param);
-    sleep(2);
-    delete param;
-    return nullptr;
-}
 
 int main()
 {
     std::cout << "-------------" << std::endl;
-
-    ThreadPool pool(4);
-    for (int i = 0; i < 10; i++) {
-        std::cout << i << std::endl;
-        int *arg = new int(i);
-        pool.addTask(task, arg);
-    }
-
     std::cout << "-------------" << std::endl;
 }
